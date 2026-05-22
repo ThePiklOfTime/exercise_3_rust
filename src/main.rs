f1432c251e00a5980ab73767d826ed0030c814e5
ThePiklOfTime <nicotolvanen@gmail.com>
2026-05-22 19:42:14 +0300

    added checking for negative numbers and zero

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 fn main() {
    let mut x: i32 = 0;
+   println!("By how much do you want to increment the number?");
     while  x < 32767{
-        println!("By how much do you want to increment the number?");
+        
         if x < 32767 {
             println!("Current: {}. Increment by:", x);
         }
         let mut increment = String::new();
         std::io::stdin().read_line(&mut increment).expect("Failed to read line");
         let increment: i32 = increment.trim().parse().expect("Please type a number!");
+        if increment < 0 {
+            println!("The given value is lower than 0.");
+            continue;
+        }else if increment == 0 {
+            break;
+        }
         x += increment;
         
         
